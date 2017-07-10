51a2e228881dbb45d331d3e0ed3dd138382e9258
Zeus El Capitan <zeuselcapitan@zeuss-mac-mini-el-capitan.local>
2017-07-10 12:56:46 +0300

    Initial Commit

---
+++ ServerApplication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ServerApplication
+//
+//  Created by Zeus El Capitan on 7/10/17.
+//  Copyright © 2017 Zeus El Capitan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ServerApplication/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ServerApplication
+//
+//  Created by Zeus El Capitan on 7/10/17.
+//  Copyright © 2017 Zeus El Capitan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
